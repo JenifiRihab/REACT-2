d26947a86b6af75c1647fb69628091bc135a48cf
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-06-28 10:33:11 +0200

    go to the REACT2

---
+++ react-exo-install/src/ArticlesList.jsx
@@ -0,0 +1,44 @@
+// const ArticlesList = () =>{
+//     return (
+//         <div>
+//             <article>
+//                 <h2> Article 1</h2>
+//                 <p> contenue article 1</p>
+//                 <img src="https://ae01.alicdn.com/kf/HTB12kNOaZfrK1Rjy0Fmq6xhEXXaI/Landscape-Mural-Photo-Wallpapers-City-Night-for-Living-Room-Bedroom-Wallcoverings-Wall-Art-Decor-Wall-Covering.jpg_640x640.jpg" alt="random" />
+//             </article>
+
+//             <article>
+//                 <h2> Article 2</h2>
+//                 <p> contenue article 2</p>
+//                 <img src="https://ae01.alicdn.com/kf/HTB12kNOaZfrK1Rjy0Fmq6xhEXXaI/Landscape-Mural-Photo-Wallpapers-City-Night-for-Living-Room-Bedroom-Wallcoverings-Wall-Art-Decor-Wall-Covering.jpg_640x640.jpg" alt="random" />
+//             </article>
+
+//             <article>
+//                 <h2> Article 3</h2>
+//                 <p> contenue article 3</p>
+//                 <img src="https://ae01.alicdn.com/kf/HTB12kNOaZfrK1Rjy0Fmq6xhEXXaI/Landscape-Mural-Photo-Wallpapers-City-Night-for-Living-Room-Bedroom-Wallcoverings-Wall-Art-Decor-Wall-Covering.jpg_640x640.jpg" alt="random" />
+//             </article>
+//         </div>
+//     );
+// }
+
+
+
+// -----------------------------------------------------------------
+
+
+
+
+const ArticlesList = () =>{
+    return (
+        <div className="App">
+            <article>
+                <h2> Les photos de voyage qui font rêver</h2>
+                <p> Imaginez un peignoir jeté sur une chaise longue savamment placée aux abords de la piscine d’un hôtel chic, dont la terrasse surplombe la mer…</p>
+                <img src="https://www.selection.ca/wp-content/uploads/2021/03/photos-de-voyage-piscine-debordement-hotel-reve.jpg" alt="random" />
+            </article>
+        </div>
+    );
+}
+
+export default ArticlesList;
