f33011a4a190cd316686d60a8ef52f3f077b56ba
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-07-10 14:46:28 +0200

    go to the REACT-EXO-INSTALL

--- 07-react-exo-install/src/Coworking.jsx
+++ 07-react-exo-install/src/Coworking.jsx
@@ -1,14 +1,14 @@
-const Coworking = ({element}) =>{
+const Coworking = ({el}) =>{
     
     return(
 
-        <div className="Article">
+        <div >
 
-            <h2>{element.name}</h2>
+            <h2>{el.name}</h2>
             <img className="logo2" src="https://th.bing.com/th/id/R.dcbf865113333aa49d212103e467ec92?rik=I8p%2b22S%2bQoBLjQ&pid=ImgRaw&r=0" alt="" />
-            <p>Address : {element.address}</p>
-            <p>City : {element.city}</p>
-            <p>Note : {element.rating}</p>
+            <p>Address : {el.address}</p>
+            <p>City : {el.city}</p>
+            <p>Note : {el.rating}</p>
 
         </div>
     )
--- 07-react-exo-install/src/MinCoworking.jsx
+++ 07-react-exo-install/src/MinCoworking.jsx
@@ -5,7 +5,7 @@ const MinCoworking = ({ coworkingsFromFakeApi }) => {
   return (
     <div className="App-coworking">
         <h1>  les coworkings possédant une note minimum de 4/5</h1>
-        {coworkingsFromFakeApi.map((element) => element.rating >= 4 && <Coworking element={element} />)}
+        {coworkingsFromFakeApi.map((el) => el.rating >= 4 && <Coworking el={el} />)}
    
     </div>
     
