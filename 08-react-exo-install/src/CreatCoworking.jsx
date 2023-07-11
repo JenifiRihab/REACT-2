c028f566c5eed03069fde6138b8f4a4b0e08e128
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-07-11 14:17:56 +0200

    go to the REACT-EXO-INSTALL

--- 08-react-exo-install/src/CreatCoworking.jsx
+++ 08-react-exo-install/src/CreatCoworking.jsx
@@ -1,32 +1,97 @@
-const CreateCoworking =() =>{
-    const handleSubmit =(event) => {
+// const CreateCoworking =() =>{
+//     const handleSubmit =(event) => {
+//         event.preventDefault();
+
+//         const title = event.target.title.value;
+//         const description = event.target.description.value;
+//         const address = event.target.address.value;
+//              console.log(title + " " + description + " " + address);
+
+//     };
+    
+//     return (
+
+//         <form className="form" onSubmit={handleSubmit}>
+
+//             <label>Titre</label>
+//                 <input type="text" name="title" /> 
+//     <br />
+//     <hr />
+//             <label>Description</label>
+//                 <textarea name="description" cols="30" rows="5"></textarea>
+//        <br /> 
+//        <hr />
+//             <label>Adresse</label>
+//                 <input type="text" name="address" />
+//         <br />
+//         <hr />
+//             <button  className="btn"  type="submit">Créer</button>
+
+//         </form>
+
+//       );
+//     };
+    
+//     export default CreateCoworking;
+
+// _________________________________________________________________
+
+ import { useState } from "react";
+
+    const CreateCoworking = () => {
+      
+      const [title, setTitle] = useState("");
+      const [description, setDescription] = useState("");
+      const [address, setAddress] = useState("");
+    
+      const handleSubmit = (event) => {
         event.preventDefault();
-
-        const title = event.target.title.value;
-        const description = event.target.description.value;
-        const address = event.target.address.value;
-             console.log(title + " " + description + " " + address);
-
-    };
+    
+        setTitle(event.target.title.value);
+        setDescription(event.target.description.value);
+        setAddress(event.target.address.value);
+      };
     
     return (
 
+    <div>
         <form className="form" onSubmit={handleSubmit}>
 
             <label>Titre</label>
                 <input type="text" name="title" /> 
-        
+        <br />
+        <hr />
+            
             <label>Description</label>
                 <textarea name="description" cols="30" rows="5"></textarea>
-        
+        <br /> 
+        <hr />
+            
             <label>Adresse</label>
                 <input type="text" name="address" />
-        
+        <br />
+        <hr />
+            
             <button  className="btn"  type="submit">Créer</button>
 
         </form>
+
+        <div className="result">
+            <p>RESULT : </p>
+            <p>TITLE : {title}</p>
+            <p>DESCRIPTION : {description}</p>
+            <p>ADDRESS : {address}</p>
+        </div>
+
+  </div>
 
       );
     };
     
     export default CreateCoworking;
+
+
+
+
+
+    
